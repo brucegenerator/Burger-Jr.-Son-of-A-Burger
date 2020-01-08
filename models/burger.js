5e5d5f6a89015d946ee24971e2f75f93569d200c
Jonathan Roth <rothje@vcu.edu>
2020-01-07 21:40:33 -0500

    sequelized the burger

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,15 @@
+var Sequelize = require('sequelize');
 
-module.exports = function(sequelize, DataTypes) {
-    var Burger = sequelize.define("Burger", {
+
+const Burger = sequelize.define("Burger", {
         burger_name:{
-            type: DataTypes.STRING,
+            type: Sequelize.STRING,
             allowNull: false
         },
         devoured:{
-            type: DataTypes.BOOLEAN,
+            type: Sequelize.BOOLEAN,
             defaultValue: false,
             allowNull: false            
         }
     });
-    return Burger;
-}
+module.exports = Burger;
